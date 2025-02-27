38f74230e460d09b2c5a5b5557c26b8bff9eef47
Gilbert Young Jr <gilbertjyoungjr@gmail.com>
2025-02-27 07:37:08 -0600

    update portfolio theme

---
+++ app/data.ts
@@ -0,0 +1,101 @@
+type Project = {
+  name: string
+  description: string
+  link: string
+  video: string
+  id: string
+}
+
+type WorkExperience = {
+  company: string
+  title: string
+  start: string
+  end: string
+  link: string
+  id: string
+}
+
+type BlogPost = {
+  title: string
+  description: string
+  link: string
+  uid: string
+}
+
+type SocialLink = {
+  label: string
+  link: string
+}
+
+export const PROJECTS: Project[] = [
+  {
+    name: 'Motion Primitives Pro',
+    description:
+      'Advanced components and templates to craft beautiful websites.',
+    link: 'https://pro.motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
+    id: 'project1',
+  },
+  {
+    name: 'Motion Primitives',
+    description: 'UI kit to make beautiful, animated interfaces.',
+    link: 'https://motion-primitives.com/',
+    video:
+      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
+    id: 'project2',
+  },
+]
+
+export const WORK_EXPERIENCE: WorkExperience[] = [
+  {
+    company: 'Hyatt Hotels Corporation',
+    title: 'Senior Software Engineer, Data',
+    start: 'July 2023',
+    end: 'Dec 2024',
+    link: 'https://www.hyatt.com/',
+    id: 'work1',
+  },
+  {
+    company: 'Post News',
+    title: 'Senior Software Engineer, AI',
+    start: 'Dec 2022',
+    end: 'Jun 2023',
+    link: 'https://techcrunch.com/2024/04/19/post-news-the-a16z-funded-twitter-alternative-is-shutting-down/',
+    id: 'work2',
+  },
+  {
+    company: 'Serverless Guru',
+    title: 'Senior Backend Engineer - Contractor',
+    start: 'Jan 2022',
+    end: 'Feb 2023',
+    link: 'https://www.serverlessguru.com/',
+    id: 'work3',
+  },
+]
+
+export const BLOG_POSTS: BlogPost[] = [
+  {
+    title: 'Generative AI: What is it and why you should care?',
+    description: 'Generative AI: What is it and why you should care?',
+    link: '/blog/gen-ai',
+    uid: 'blog-3',
+  },
+]
+
+export const SOCIAL_LINKS: SocialLink[] = [
+  {
+    label: 'Github',
+    link: 'https://github.com/gjyoungjr',
+  },
+  {
+    label: 'Twitter',
+    link: 'https://x.com/gjyoungjr',
+  },
+  {
+    label: 'LinkedIn',
+    link: 'https://www.linkedin.com/in/gjyoungjr/',
+  },
+]
+
+export const EMAIL = 'gilbertjyoungjr@gmail.com'
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,38 +1,56 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
-import { ToggleTheme } from "@/components/toggle-theme";
+import type { Metadata, Viewport } from 'next'
+import { Geist, Geist_Mono } from 'next/font/google'
+import './globals.css'
+import { Header } from './header'
+import { Footer } from './footer'
+import { ThemeProvider } from 'next-themes'
 
-const inter = Inter({ subsets: ["latin"] });
+export const viewport: Viewport = {
+  width: 'device-width',
+  initialScale: 1,
+  themeColor: '#ffffff',
+}
 
 export const metadata: Metadata = {
-  title: "Gilbert Young Jr.",
-  description: "Portfolio",
-};
+  title: 'Gilbert Young Jr.',
+  description: 'Personal portfolio.',
+}
+
+const geist = Geist({
+  variable: '--font-geist',
+  subsets: ['latin'],
+})
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+})
 
 export default function RootLayout({
   children,
-}: {
-  children: React.ReactNode;
-}) {
+}: Readonly<{
+  children: React.ReactNode
+}>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>
+      <body
+        className={`${geist.variable} ${geistMono.variable} bg-white tracking-tight antialiased dark:bg-zinc-950`}
+      >
         <ThemeProvider
+          enableSystem={true}
           attribute="class"
-          defaultTheme="dark"
-          enableSystem
-          disableTransitionOnChange
+          storageKey="theme"
+          defaultTheme="system"
         >
-          <div className="ml-5 mt-10">
-            {children}
-            <div className="fixed bottom-4 right-4">
-              <ToggleTheme />
+          <div className="flex min-h-screen w-full flex-col font-[family-name:var(--font-inter-tight)]">
+            <div className="relative mx-auto w-full max-w-screen-sm flex-1 px-4 pt-20">
+              <Header />
+              {children}
+              <Footer />
             </div>
           </div>
         </ThemeProvider>
       </body>
     </html>
-  );
+  )
 }
